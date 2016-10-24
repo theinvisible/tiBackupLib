c256083872cf79e793a83ab895661b4911d1e85f
Rene Hadler <rene@hadler.me>
2016-10-24 15:57:55 +0200

    Fixed typo in deploy_qt5 script

--- deploy_release_qt5.sh
+++ deploy_release_qt5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm /home/rene/DEV/lib/libtiBackupLib.s*
-cp -a /home/rene/DEV/qtcreator/build-tiBackupLib-Deskto_Qt_5*-Release/libtiBackupLib.s* /home/rene/DEV/lib/.
+cp -a /home/rene/DEV/qtcreator/build-tiBackupLib-Desktop_Qt_5*-Release/libtiBackupLib.s* /home/rene/DEV/lib/.
