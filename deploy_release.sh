5126e2f27940df01eeae816dc2c59b22564b3e1e
Rene Hadler <rene@hadler.me>
2026-06-13 20:00:50 +0200

    Remove obsolete deployment scripts for `libtiBackupLib`.

--- deploy_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm /home/rene/DEV/lib/libtiBackupLib.s*
-cp -a /home/rene/DEV/qtcreator/build-tiBackupLib-Deskto*-Release/libtiBackupLib.s* /home/rene/DEV/lib/.
--- deploy_release_qt5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm /home/$USER/DEV/lib/libtiBackupLib.s*
-cp -a /home/$USER/DEV/qtcreator/build-tiBackupLib-Desktop-Release/libtiBackupLib.s* /home/$USER/DEV/lib/.
